c8949b21820d31815004a61cf688efdc901bb314
Quang Do <quang.do.se@gmail.com>
2021-01-29 14:56:31 -0700

    WIP Update

--- keyboards/1upkeyboards/1up60hse/keymaps/qdo-se/keymap.c
+++ keyboards/1upkeyboards/1up60hse/keymaps/qdo-se/keymap.c
@@ -21,6 +21,11 @@ enum qd_keycodes {
   QD_M                //default: M, ctrl: enter
 };
 
+#define _BASE 0
+#define _FUNCTION 1
+#define _RGB 2
+#define _ALT 3
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* L0 - Qwerty
    * ,-----------------------------------------------------------------------------------------.
@@ -64,27 +69,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------------'
    */
 
-  [0] = LAYOUT_60_ansi(
+  [_BASE] = LAYOUT_60_ansi(
                        QD_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
                        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
                        KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_RCTL,
                        KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     QD_M,     KC_COMM,  KC_DOT ,  KC_SLSH,                      KC_RSFT,
                        KC_CAPS,  KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  KC_RGUI,  MO(1),              KC_ENT
                        ),
-  [1] = LAYOUT_60_ansi(
+  [_FUNCTION] = LAYOUT_60_ansi(
                        KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,
                        _______,  _______,  KC_UP,    _______,  _______,  _______,  _______,  _______,  KC_UP,    _______,  KC_PGUP,  KC_HOME,  _______,  _______,
                        _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  KC_END,             _______,
                        _______,  KC_VOLU,  KC_VOLD,  KC_MUTE,  _______,  _______,  NK_TOGG,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,                      _______,
                        _______,  _______,  _______,                      _______,                                _______,  MO(2),    _______,            _______
                        ),
-  [2] = LAYOUT_60_ansi(
+  [_RGB] = LAYOUT_60_ansi(
                        _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  RESET,
                        BL_TOGG, BL_INC,   BL_DEC,   BL_STEP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
                        RGB_TOG, RGB_MOD,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_SPI,  RGB_M_P,  RGB_M_B,  RGB_M_R,  RGB_M_SW, _______,  _______,            _______,
                        _______, RGB_RMOD, RGB_HUD,  RGB_SAD,  RGB_VAD,  RGB_SPD,  RGB_M_SN, RGB_M_K,  RGB_M_X,  RGB_M_G,  _______,                      _______,
                        _______, _______,  _______,                      _______,                                _______,  _______,  _______,            _______
                        ),
+  [_ALT] = LAYOUT_60_ansi(
+                       TG(0),   TG(1),    TG(2),    TG(3),    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                       _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                       _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+                       _______, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
+                       _______, _______,  _______,                      _______,                                _______,  _______,  _______,            _______
+                       )
 };
 
 // Runs just one time when the keyboard initializes.
