873a935e06dd550341ff1c96f209d6caf28101e5
Quang Do <quang.do.se@gmail.com>
2021-01-29 18:25:50 -0700

    Add debug

--- keyboards/1upkeyboards/1up60hse/keymaps/qdo-se/keymap-debug.c
+++ keyboards/1upkeyboards/1up60hse/keymaps/qdo-se/keymap-debug.c
@@ -15,11 +15,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "print.h"
 
 enum qd_keycodes {
-  QD_ESC,             // default: escape,  shift: ~
-  QD_M,               // default: M, ctrl: enter
-  QD_ALT              // layout switch
+  QD_ESC,             //default: escape,  shift: ~
+  QD_M,                //default: M, ctrl: enter
+  QD_ALT
 };
 
 #define _BASE 0
@@ -51,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-----------------------------------------------------------------------------------------+
    * |           |VolUp|VolDn|VolMu|     |     | NK  |VolMu|VolDn|VolUp|     |                 |
    * |-----------------------------------------------------------------------------------------+
-   * |       |       |       |                                 |QD_ALT |  L2   |       |       |
+   * |       |       |       |                                 |       |  L2   |       |       |
    * `-----------------------------------------------------------------------------------------'
    */
 
@@ -105,12 +106,45 @@ void matrix_scan_user(void) {
 #define MODS_ALT  (get_mods() & MOD_BIT(KC_LALT) || get_mods() & MOD_BIT(KC_RALT))
 #define MODS_GUI  (get_mods() & MOD_BIT(KC_LGUI) || get_mods() & MOD_BIT(KC_RGUI))
 
+void keyboard_post_init_user(void) {
+  // Customise these values to desired behaviour
+  debug_enable=true;
+  debug_matrix=true;
+  debug_keyboard=true;
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   static uint16_t kc;
   static bool switching_layout = false;
   static uint16_t current_layout = 0;
   static uint16_t saved_layout = 0;
 
+
+#ifdef CONSOLE_ENABLE
+  uprintf("KL: kc: 0x%04X, col: %u, row: %u, pressed: %b, time: %u, interrupt: %b, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+  if (switching_layout) {
+    print("QD_ALT is active\n");
+  } else {
+    print("QD_ALT is INactive\n");
+  }
+
+  if (layer_state_is(0)) {
+    print("Layer 0 is active\n");
+  }
+
+  if (layer_state_is(1)) {
+    print("Layer 1 is active\n");
+  }
+
+  if (layer_state_is(2)) {
+    print("Layer 2 is active\n");
+  }
+
+  if (layer_state_is(3)) {
+    print("Layer 3 is active\n");
+  }
+#endif
+  
 
   switch (keycode) {
 
@@ -158,10 +192,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
   case MO(1):
     if (record->event.pressed) {
+      print("MO(1) is activated\n");
       saved_layout = current_layout;
       layer_move(1);
     } else {
       if (saved_layout == current_layout) {
+        print("MO(1) clears layout\n");
         layer_move(current_layout);
       } 
     }
@@ -206,4 +242,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   default:
     return true; //Process all other keycodes normally
   }
+  // layer_state_is(1)
+  // layer_off(1)
 }
