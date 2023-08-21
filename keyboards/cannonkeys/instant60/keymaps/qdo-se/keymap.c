ba0f182abfe4acfcb0b74c95aff64535de29e770
Quang Do <quang.do.se@gmail.com>
2023-08-20 23:50:45 -0600

    WIP

--- keyboards/cannonkeys/instant60/keymaps/qdo-se/keymap.c
+++ keyboards/cannonkeys/instant60/keymaps/qdo-se/keymap.c
@@ -43,12 +43,23 @@ enum {
     TD_FUNCTION_SWITCH
 };
 
+void mo_function_switch(qk_tap_dance_state_t *state, void *user_data) {
+  if (state->count >= 2) {
+      layer_move(_SWITCH);
+  } else {
+      layer_move(_FUNCTION);
+  }
+}
+
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_RCTL_ENT]  = ACTION_TAP_DANCE_DOUBLE(KC_RCTL, KC_ENT),
     [TD_LSFT_CAPS]  = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-    [TD_FUNCTION_SWITCH]  = ACTION_TAP_DANCE_DOUBLE(MO(_FUNCTION), MO(_SWITCH))
+    [TD_FUNCTION_SWITCH]  = ACTION_TAP_DANCE_FN(mo_function_switch)
 };
+
+
+
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -99,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_TAB,            KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
                              KC_LCTL,           KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            TD(TD_RCTL_ENT),
                              TD(TD_LSFT_CAPS),  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT ,  KC_SLSH,                      KC_RSFT,
-                             MO(_FUNCTION),     KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  KC_RGUI,  MO(_SWITCH),        MO(_FUNCTION)
+                             MO(_FUNCTION),     KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  KC_RGUI,  MO(_SWITCH),        TD(TD_FUNCTION_SWITCH)
                              ),
 
     [_FUNCTION] = LAYOUT_60_ansi(
