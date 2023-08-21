e768e76459cb626a27302f5c0088c0b8abb13648
Quang Do <quang.do.se@gmail.com>
2023-08-20 22:44:54 -0600

    Update

--- keyboards/cannonkeys/instant60/keymaps/qdo-se/keymap.c
+++ keyboards/cannonkeys/instant60/keymaps/qdo-se/keymap.c
@@ -96,31 +96,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_TAB,            KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
                              KC_LCTL,           KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            TD(TD_RCTL_ENT),
                              TD(TD_LSFT_CAPS),  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT ,  KC_SLSH,                      KC_RSFT,
-                             MO(_FUNCTION),     KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  KC_RGUI,  MO(_SWITCH),        MO(_FUNCTION)
+                             MO(1),     KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  KC_RGUI,  MO(4),        MO(1)
                              ),
 
     [_FUNCTION] = LAYOUT_60_ansi(
                                  KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,
                                  _______,  _______,  KC_UP,    _______,  _______,  _______,  _______,  _______,  KC_UP,    _______,  KC_PGUP,  KC_HOME,  _______,  _______,
                                  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  KC_END,             _______,
-                                 _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_UP,                        MO(_SWITCH),
-                                 _______,  _______,  _______,                      _______,                                KC_LEFT,  KC_DOWN,  KC_RGHT,            _______
+                                 _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_UP,                        _______,
+                                 _______,  _______,  _______,                      _______,                                KC_LEFT,  KC_DOWN,  KC_RGHT,            MO(4)
                                  ),
 
     [_RGB] = LAYOUT_60_ansi(
                             _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT,
                             BL_TOGG,  BL_INC,   BL_DEC,   BL_STEP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
                             _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-                            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      MO(_SWITCH),
-                            _______,  _______,  _______,                      _______,                                _______,  _______,  _______,            _______
+                            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
+                            _______,  _______,  _______,                      _______,                                _______,  _______,  _______,            MO(4)
                             ),
 
     [_ENTERTAINMENT] = LAYOUT_60_ansi(
                                       KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
                                       _______,  _______,  KC_UP,    _______,  _______,  _______,  _______,  _______,  KC_UP,    _______,  KC_PGUP,  KC_HOME,  _______,  _______,
                                       _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  KC_END,             _______,
-                                      _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_UP,                        MO(_SWITCH),
-                                      _______,  _______,  _______,                      _______,                                KC_LEFT,  KC_DOWN,  KC_RGHT,            _______ 
+                                      _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_UP,                        _______,
+                                      _______,  _______,  _______,                      _______,                                KC_LEFT,  KC_DOWN,  KC_RGHT,            MO(4)
                                       ),
 
 
@@ -128,8 +128,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                QL_BASE,  QL_FUNCTION, QL_RGB,   QL_ENTERTAINMENT, _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT,
                                _______,  _______,     _______,  _______,          _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
                                _______,  _______,     _______,  _______,          _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            QL_ENTERTAINMENT,
-                               _______,  _______,     _______,  _______,          _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,                      MO(_SWITCH),
-                               _______,  _______,     _______,                              _______,                                _______,  _______,  _______,            _______ 
+                               _______,  _______,     _______,  _______,          _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,                      _______,
+                               _______,  _______,     _______,                              _______,                                _______,  _______,  _______,            MO(4)
                                ),
 };
 
@@ -167,41 +167,41 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         return false;
 
-    case QL_BASE:
-        if (record->event.pressed) {
-            layer_move(_BASE);
-            backlight_level(_BASE);
-            backlight_disable();
-            return false;
-        }
-        return true;
+    /* case QL_BASE: */
+    /*     if (record->event.pressed) { */
+    /*         layer_move(_BASE); */
+    /*         backlight_level(_BASE); */
+    /*         backlight_disable(); */
+    /*         return false; */
+    /*     } */
+    /*     return true; */
 
-    case QL_FUNCTION:
-        if (record->event.pressed) {
-            layer_move(_FUNCTION);
-            backlight_level(_FUNCTION);
-            backlight_enable();
-            return false;
-        }
-        return true;
+    /* case QL_FUNCTION: */
+    /*     if (record->event.pressed) { */
+    /*         layer_move(_FUNCTION); */
+    /*         backlight_level(_FUNCTION); */
+    /*         backlight_enable(); */
+    /*         return false; */
+    /*     } */
+    /*     return true; */
 
-    case QL_RGB:
-        if (record->event.pressed) {
-            layer_move(_RGB);
-            backlight_level(_RGB);
-            backlight_enable();
-            return false;
-        }
-        return true;
+    /* case QL_RGB: */
+    /*     if (record->event.pressed) { */
+    /*         layer_move(_RGB); */
+    /*         backlight_level(_RGB); */
+    /*         backlight_enable(); */
+    /*         return false; */
+    /*     } */
+    /*     return true; */
 
-    case QL_ENTERTAINMENT:
-        if (record->event.pressed) {
-            layer_move(_ENTERTAINMENT);
-            backlight_level(_ENTERTAINMENT);
-            backlight_enable();
-            return false;
-        }
-        return true;
+    /* case QL_ENTERTAINMENT: */
+    /*     if (record->event.pressed) { */
+    /*         layer_move(_ENTERTAINMENT); */
+    /*         backlight_level(_ENTERTAINMENT); */
+    /*         backlight_enable(); */
+    /*         return false; */
+    /*     } */
+    /*     return true; */
 
     default:
         return true; //Process all other keycodes normally
