dea497d0af65b101a9679bce41af80cb1805f560
Quang Do <quang.do.se@gmail.com>
2023-08-20 21:10:36 -0600

    Update

--- keyboards/cannonkeys/instant60/keymaps/qdo-se/keymap.c
+++ keyboards/cannonkeys/instant60/keymaps/qdo-se/keymap.c
@@ -19,14 +19,18 @@
 enum qd_keycodes {
     QD_ESC,             // default: escape,  shift: ~
     QD_M,               // default: M, ctrl: enter
-    QD_ALT              // layout switch
+    QD_L0,              // layout 0
+    QD_L1,              // layout 1
+    QD_L2,              // layout 2
+    QD_L3,              // layout 3
 };
 
 enum layers {
     _BASE,
     _FUNCTION,
+    _RGB,
     _ENTERTAINMENT,
-    _RGB
+    _SWITCH
 };
 
 //Tap Dance Declarations
@@ -90,30 +94,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_TAB,            KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
                              KC_LCTL,           KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            TD(TD_RCTL_ENT),
                              TD(TD_LSFT_CAPS),  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT ,  KC_SLSH,                      KC_RSFT,
-                             MO(1),             KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  KC_RGUI,  MO(1),              KC_ENT
+                             MO(1),             KC_LGUI,  KC_LALT,                      KC_SPC,                                 KC_RALT,  KC_RGUI,  MO(4),              MO(1)
                              ),
+    
     [_FUNCTION] = LAYOUT_60_ansi(
                                  KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_DEL,
                                  _______,  _______,  KC_UP,    _______,  _______,  _______,  _______,  _______,  KC_UP,    _______,  KC_PGUP,  KC_HOME,  _______,  _______,
                                  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  KC_END,             _______,
-                                 _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,                      _______,
-                                 _______,  _______,  _______,                      _______,                                QD_ALT,   MO(2),    _______,            _______
+                                 _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_UP,                        MO(2),
+                                 _______,  _______,  _______,                      _______,                                KC_LEFT,  KC_DOWN,  KC_RGHT,            _______
                                  ),
 
     [_RGB] = LAYOUT_60_ansi(
-                            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT,
+                            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
                             BL_TOGG,  BL_INC,   BL_DEC,   BL_STEP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
                             _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
                             _______,  _______,  _______,  _______,  _______,  _______,  NK_TOGG,  _______,  _______,  _______,  _______,                      _______,
                             _______,  _______,  _______,                      _______,                                _______,  _______,  _______,            _______
                             ),
+    
     [_ENTERTAINMENT] = LAYOUT_60_ansi(
                                       KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
                                       _______,  _______,  KC_UP,    _______,  _______,  _______,  _______,  _______,  KC_UP,    _______,  KC_PGUP,  KC_HOME,  _______,  _______,
                                       _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  KC_END,             _______,
-                                      _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,                      _______,
-                                      _______,  _______,  _______,                      _______,                                QD_ALT,   MO(2),    _______,            _______
+                                      _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_UP,                        MO(2),
+                                      _______,  _______,  _______,                      _______,                                KC_LEFT,  KC_DOWN,  KC_RGHT,            _______
                                       ),
+    
+    [_SWITCH] = LAYOUT_60_ansi(
+                               QD_L0,    QD_L1,    QD_L2,    QD_L3,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  QK_BOOT,
+                               _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+                               _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+                               _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      QD_L3,
+                               _______,  _______,  _______,                      _______,                                _______,  _______,  _______,            _______
+                               ),
 };
 
 // Runs just one time when the keyboard initializes.
@@ -131,23 +145,12 @@ void matrix_scan_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     static uint16_t kc;
-    static bool switching_layout = false;
-    static uint16_t current_layout = 0;
-    static uint16_t previous_layout_mo1 = 0;
-    static uint16_t ENTERTAINMENT_LAYOUT = 4;
-
 
     switch (keycode) {
 
     case QD_ESC:
         if (record->event.pressed) {
-            if (switching_layout) {
-                current_layout = 0;
-                layer_move(current_layout);
-                return false;
-            }
-
-            if (MODS_GUI) {
+              if (MODS_GUI) {
                 kc = KC_ESCAPE;
             } else if (MODS_SHIFT) {
                 kc = KC_TILDE;
@@ -187,93 +190,62 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         }
         return false;
 
-    case MO(1):
-        /**
-         * Override default MO(1) behavior to allow to change layout.
-         *
-         * If current layout is not changed during pressing MO(1), reverse back to previous layout once releasing MO(1). Just like default MO(1) behavior.
-         * However, if current layout is changed, keep the current layout.
-         *
-         * For example:
-         * If current layout is 2, pressing MO(1) will move to layout 1. If current layout is not changed, releasing MO(1) will move back to current layout 2.
-         * If current layout is 2, pressing MO(1) will move to layout 1. If current layout is changed to 3, releasing MO(1) will do NOTHING and current layout is 3.
-         *
-         */
+    /* case MO(1): */
+    /*     /\** */
+    /*      * Override default MO(1) behavior to allow to change layout. */
+    /*      * */
+    /*      * If current layout is not changed during pressing MO(1), reverse back to previous layout once releasing MO(1). Just like default MO(1) behavior. */
+    /*      * However, if current layout is changed, keep the current layout. */
+    /*      * */
+    /*      * For example: */
+    /*      * If current layout is 2, pressing MO(1) will move to layout 1. If current layout is not changed, releasing MO(1) will move back to current layout 2. */
+    /*      * If current layout is 2, pressing MO(1) will move to layout 1. If current layout is changed to 3, releasing MO(1) will do NOTHING and current layout is 3. */
+    /*      * */
+    /*      *\/ */
+    /*     if (record->event.pressed) { */
+    /*         previous_layout_mo1 = current_layout; */
+    /*         layer_move(1); */
+    /*     } else { */
+    /*         if (previous_layout_mo1 == current_layout) { */
+    /*             layer_move(previous_layout_mo1); */
+    /*         } */
+    /*     } */
+
+    /*     return false; */
+
+    case QD_L0:
         if (record->event.pressed) {
-            previous_layout_mo1 = current_layout;
+            layer_move(0);
+            backlight_level(0);
+            backlight_disable();
+            return false;
+        }
+        return true;
+
+    case QD_L1:
+        if (record->event.pressed) {
             layer_move(1);
-        } else {
-            if (previous_layout_mo1 == current_layout) {
-                layer_move(previous_layout_mo1);
-            }
-        }
-
-        return false;
-
-    case QD_ALT:
-        if (record->event.pressed) {
-            switching_layout = true;
-        } else {
-            switching_layout = false;
-        }
-        return false;
-
-    case KC_ESC:
-    case KC_GRV:
-        if (record->event.pressed) {
-            if (switching_layout) {
-                current_layout = 0;
-                layer_move(current_layout);
-                backlight_level(0);
-                backlight_disable();
-                return false;
-            }
+            backlight_level(1);
+            backlight_enable();
+            return false;
         }
         return true;
 
-    case KC_RSFT:
-        // Toggle ENTERTAINMENT_LAYOUT
+    case QD_L2:
         if (record->event.pressed) {
-            if (switching_layout) {
-                if (current_layout == ENTERTAINMENT_LAYOUT) {
-                    current_layout = 0;
-                    backlight_level(0);
-                    backlight_disable();
-                } else {
-                    current_layout = ENTERTAINMENT_LAYOUT;
-                    backlight_level(1);
-                    backlight_enable();
-                }
-
-                layer_move(current_layout);
-                return false;
-            }
+            layer_move(2);
+            backlight_level(2);
+            backlight_enable();
+            return false;
         }
         return true;
 
-    case KC_1:
-    case KC_F1:
+    case QD_L3:
         if (record->event.pressed) {
-            if (switching_layout) {
-                current_layout = 1;
-                layer_move(current_layout);
-                backlight_level(1);
-                backlight_enable();
-                return false;
-            }
-        }
-        return true;
-
-    case KC_2:
-    case KC_F2:
-        if (record->event.pressed) {
-            if (switching_layout) {
-                current_layout = 2;
-                layer_move(current_layout);
-                backlight_level(2);
-                backlight_enable();
-                return false;
-            }
+            layer_move(3);
+            backlight_level(3);
+            backlight_enable();
+            return false;
         }
         return true;
 
